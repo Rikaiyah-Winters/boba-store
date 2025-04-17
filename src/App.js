c2025e015c3012248773720365363d9262544bc3
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-04-17 11:27:08 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import DrinkCategory from './components/DrinkCategory';
 
 function App() {
+  //const [flavorCategory, setFlavorCategory] = useState(null);
+
+  const drinkCategories = [
+    {
+      key: 1,
+      title: "tea",
+      img: "https://plus.unsplash.com/premium_vector-1712005192222-53ed25b5a490?q=80&w=2368&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      key: 2,
+      title: "lemonade",
+      img: "https://plus.unsplash.com/premium_vector-1733312371246-8301e7fc42e0?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {drinkCategories.map((category) => (
+        <DrinkCategory category={category} key={category.key}/>
+      ))}
     </div>
   );
 }
---
+++ src/components/DrinkCategory.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DrinkCategory = ({ category }) => {
+    return (
+        <article className="category-card">
+            <h2>{category.title}</h2>
+            <figure className="category-figure">
+                <img src={category.img} alt={category.category} />
+            </figure>
+            <button>Add Flavor</button>
+        </article>
+    )
+}
+
+export default DrinkCategory;
