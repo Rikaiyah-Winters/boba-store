9d9a068dc125b77b0997a2eee9a2925f7ffb02f6
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-05-01 09:20:24 -0500

    Added orders array and commented idea for indiv drink orders

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Cart = () => {
+    let orders = [] //collects drink orders, might have to relocate this 
     return(
         <div className="cart">
             <h1>Your cart</h1>
         </div>
+        {/*(orders.map((order) => (
+            <IndividualDrnkOrder />
+          )))*/}
+
     )
 }
 
