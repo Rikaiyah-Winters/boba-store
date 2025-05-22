ad3e787be9cf09cf3ce8df8cd1fe69538f8d82e6
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-05-22 08:32:56 -0500

    Deleted flavored tea/lemonade img

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     //{
     //key: 2,
     //title: "flavored tea/lemonade",
-    //img: "https://plus.unsplash.com/premium_vector-1733312371246-8301e7fc42e0?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //img: ""
     //}
   ];
 
